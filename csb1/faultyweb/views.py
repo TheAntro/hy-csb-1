28f3195630132af494599723b9dd753c83c67520
TheAntro <waltherbeach@gmail.com>
2025-08-21 21:27:27 +0300

    Create registration page

--- csb1/faultyweb/views.py
+++ csb1/faultyweb/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+
+from .forms import RegistrationForm
 
 def index(request):
     return HttpResponse("Home page")
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = User.objects.create_user(username, None, password)
+            user.save()
+            return redirect("/faultyweb")
+    else:
+        form = RegistrationForm
+
+    return render(request, "faultyweb/register.html", {"form": form})
